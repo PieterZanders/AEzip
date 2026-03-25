3c0cf711d35dceeaa6fb4288e73a19729750c258
PieterZanders <pieterzanders@gmail.com>
2026-03-25 01:36:22 +0100

    minor changes

--- aezip/compress.py
+++ aezip/compress.py
@@ -33,31 +33,27 @@ print("dihedral_indices_and_names")
 sliced_indices = convert_full_to_sliced_indices(traj, sliced_traj)
 print("sliced_indices")
 
+# @TODO: I wanna choose which features to use
 coord_feat = sliced_traj.xyz.reshape(len(sliced_traj.xyz), -1)
 dih_traj = calculate_dih_traj(traj, dihedral_indices_and_names)
-#dih_traj = np.concatenate((np.sin(dih_traj, dtype=float), np.cos(dih_traj, dtype=float)), axis=1)
-print("coord_feat: ", coord_feat.shape)
 
-partition_idx = sliced_traj.xyz.shape[1]*3
-print(partition_idx)
+scaler = MinMaxScaler(feature_range=(0, 1))
 
 ## Autoencoder Input
-scaler = MinMaxScaler(feature_range=(-1, 1))
-coord_feat = scaler.fit_transform(coord_feat)
-print(np.min(coord_feat), np.max(coord_feat))
-#dih_traj = scaler.fit_transform(dih_traj)
-#scaler = MinMaxScaler(feature_range=(0, 1))
-#feat = scaler.fit_transform(dih_traj) # np.concatenate((coord_feat, dih_traj), axis=1)
-print(dih_traj.shape)
-feat = dih_traj #(dih_traj+ 180) / 360
-np.save("WT_apo_ChainsA_SC_angles.npy", dih_traj)
-feat = np.stack([np.sin(np.deg2rad(feat)), np.cos(np.deg2rad(feat))], axis=-1).reshape(len(feat), -1)
-print(feat)
-print(feat.shape, np.min(feat), np.max(feat))
+if coord_feat is not None:
+    print(np.min(coord_feat), np.max(coord_feat))
+
+if dih_traj is not None:
+    print(np.min(dih_traj), np.max(dih_traj))
+
+raw_feat = np.concatenate((coord_feat, dih_traj), axis=1)
+
+feat = scaler.fit_transform(raw_feat)
 
 # Autoencoder 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# @TODO: put hyperparams in a config folder as json file
 hyperparams = {
     "input_dim": feat.shape[1],    
     "layers": 5, 
@@ -83,19 +79,19 @@ train_dataloader = torch.utils.data.DataLoader(dataset = torch.FloatTensor(feat)
                                                drop_last=False, 
                                                shuffle = False)
 
-model = SAutoEncoder(input_dim=hyperparams["input_dim"], 
+model = AutoEncoder(input_dim=hyperparams["input_dim"], 
                     nlayers=hyperparams["layers"], 
                     latent_dim=hyperparams["latent_dim"], 
                     dropout=hyperparams["dropout"], 
                     negative_slope=hyperparams["negative_slope"], 
                     batch_norm=hyperparams["batch_norm"]).to(device)
+
 optimizer =  torch.optim.Adam(model.parameters(), 
                               lr=hyperparams["learning_rate"], 
                               weight_decay=hyperparams["weight_decay"], 
                               foreach=False)
 loss_function = torch.nn.MSELoss()
-loss_function = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-#loss_function = PeriodicLoss()  # CombinedLoss(partition_idx, cartesian_weight=1.0, angular_weight=1.0)
+
 print(model)
 
 train_loss, latent = compress(model, 
